e3160d47a38d92e46246191cc8c6647409b8773a
Abhinav <abhinav03m@gmail.com>
2025-09-27 01:07:22 +0000

    fix(setup.sh)
    --Script now saves influxdb3 authentication token to .env

--- docker/setup.sh
+++ docker/setup.sh
@@ -15,12 +15,8 @@ sleep 5
 echo "▶️ Creating admin token..."
 INFLUXDB_ADMIN_TOKEN=$(sudo docker compose exec -T influxdb3 influxdb3 create token --admin | grep "Token:" | awk '{print $2}') \
   || { echo "❌ Failed to create admin token"; exit 1; }
+echo "INFLUXDB_TELEGRAF_TOKEN=${INFLUXDB_ADMIN_TOKEN}" >> .env
 echo "✅ Admin token created."
 
-# echo "▶️ Creating bucket 'metrics'..."
-# sudo docker compose exec -T influxdb influxdb3 create bucket metrics \
-#   --token "${INFLUXDB_ADMIN_TOKEN}" || { echo "❌ Failed to create bucket"; exit 1; }
-# echo "✅ Bucket 'metrics' created."
-
 echo "▶️ Starting Mosquitto and telegraf"
-sudo docker compose up -d mosquitto telegraf
+sudo docker compose up -d mosquitto telegraf explorer loki grafana
